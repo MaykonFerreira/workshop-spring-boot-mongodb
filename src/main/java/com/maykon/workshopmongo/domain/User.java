551d743c52d68d066923007c380ee1e61c4407a7
Maykon Ferreira <lmaykonf@gmail.com>
2020-08-01 18:20:21 -0300

    Referenciando os posts do usuario

--- src/main/java/com/maykon/workshopmongo/domain/User.java
+++ src/main/java/com/maykon/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.maykon.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -17,10 +20,19 @@ public class User implements Serializable {
 		private String name;
 		private String email;
 		
+		@DBRef(lazy=true)
+		private List<Post> posts = new ArrayList<>();
+		
 		public String getId() {
 			return id;
 		}
+		public List<Post> getPosts() {
+			return posts;
+		}
 
+		public void setPosts(List<Post> posts) {
+			this.posts = posts;
+		}
 		public void setId(String id) {
 			this.id = id;
 		}
@@ -76,5 +88,7 @@ public class User implements Serializable {
 				return false;
 			return true;
 		}
+
+
 		
 }
