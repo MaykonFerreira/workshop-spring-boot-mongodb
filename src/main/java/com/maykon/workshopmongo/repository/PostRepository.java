2cd2f37f10965e684225c68a4fcf22f381aeb826
Maykon Ferreira <lmaykonf@gmail.com>
2020-08-01 19:55:32 -0300

    Consulta simples com @Query

--- src/main/java/com/maykon/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/maykon/workshopmongo/repository/PostRepository.java
@@ -13,12 +13,16 @@ import com.maykon.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	// Referencia para o mongoDB search é do BD e Title é o seu Titulo
+	// modo JSon 
+	//{ <field>: { $regex: /pattern/, $options: '<options>' } }
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") 
 	List<Post> searchTitle(String text);
 	
 	// Essa linha ela ignora Ucase e Lcase
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
+	
+	// Essa consulta é muito dinamica com varias condições
 	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
 	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
