5e1e982226ea7cdab0e369088fb4d5b3fc0cf4ee
Maykon Ferreira <lmaykonf@gmail.com>
2020-08-01 15:06:54 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/maykon/workshopmongo/resources/UserResource.java
+++ src/main/java/com/maykon/workshopmongo/resources/UserResource.java
@@ -1,8 +1,8 @@
 package com.maykon.workshopmongo.resources;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maykon.workshopmongo.domain.User;
+import com.maykon.workshopmongo.dto.UserDTO;
 import com.maykon.workshopmongo.services.UserService;
 
 @RestController
@@ -23,13 +24,12 @@ public class UserResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	
-	public ResponseEntity<List<User>> findAll() {
-		List<User> list = new ArrayList<>();
-		//User maria = new User("1001", "Maria Brown", "maria@gmail.com");
-		//User alex = new User("1002", "Alex Green", "alex@gmail.com");
-		//list.addAll(Arrays.asList(maria, alex));
-		list.addAll(service.findAll());
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<User> list = service.findAll();
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		
+		//list.addAll(service.findAll());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
